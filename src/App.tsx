099b53939fac0bc074cc77996c61d59a511d92e4
Kooribo <maximilianollendorff@gmail.com>
2024-03-13 23:23:41 +0100

    feat: add mui styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,85 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Box, Grid, Link, Stack, Typography } from "@mui/material";
 
 function App() {
+	const [adapter, setAdapter] = useState(false);
+
+	useEffect(() => {
+		const getAdapter = async () => {
+			if (!("gpu" in navigator)) {
+				console.error("User agent doesn’t support WebGPU.");
+				return setAdapter(false);
+			}
+
+			const adapter = await navigator.gpu?.requestAdapter({
+				powerPreference: "high-performance",
+			});
+			const device = await adapter?.requestDevice();
+			//set context current device
+
+			console.log(adapter.requestAdapterInfo());
+			return setAdapter(true);
+		};
+		getAdapter();
+	}, []);
+
+	//https://developer.chrome.com/docs/web-platform/webgpu?hl=de
+
+	const webGpuLink =
+		"https://developer.mozilla.org/en-US/docs/Web/API/WebGPU_API#browser_compatibility";
+
 	return (
-		<>
-			<h1>Cellular Automata</h1>
-		</>
+		<Box
+			display="flex"
+			justifyContent="center"
+			alignItems="center"
+			flexDirection="column"
+			mt={3}
+		>
+			<Typography
+				variant="h2"
+				textAlign="center"
+				color="primary.main"
+				gutterBottom
+			>
+				Cellular Automata
+			</Typography>
+			<Stack sx={{ m: 2 }} gap={6}>
+				{!adapter && (
+					<Typography
+						fontSize="large"
+						border="5px solid #242424"
+						color="warning.light"
+						textAlign="center"
+					>
+						It seems like your current browser does not support{" "}
+						<Link
+							href={webGpuLink}
+							underline="hover"
+							target="_blank"
+							rel="noreferrer"
+							color="error.light"
+						>
+							WebGPU
+						</Link>
+					</Typography>
+				)}
+				<Grid container spacing={3} direction="row">
+					<Grid item xs={12} md={6}>
+						<Box sx={{ p: 2, border: "1px dashed grey" }}></Box>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<Box sx={{ p: 2, border: "1px dashed grey" }}></Box>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<Box sx={{ p: 2, border: "1px dashed grey" }}></Box>
+					</Grid>
+					<Grid item xs={12} md={6}>
+						<Box sx={{ p: 2, border: "1px dashed grey" }}></Box>
+					</Grid>
+				</Grid>
+			</Stack>
+		</Box>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,40 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const darkTheme = createTheme({
+	palette: {
+		primary: {
+			main: "rgba(255, 255, 255, 0.87)",
+		},
+		background: {
+			default: "#1f1f1f",
+		},
+	},
+	typography: {
+		fontFamily: "Inter, Ubuntu",
+	},
+});
+
+//TODO add light theme later
+const lightTheme = createTheme({
+	palette: {
+		primary: {
+			main: "#1f1f1f",
+		},
+		background: {
+			default: "#E3E6E6",
+		},
+	},
+	typography: {
+		fontFamily: "Ubuntu",
+	},
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<ThemeProvider theme={darkTheme}>
+		<CssBaseline />
+		<App />
+	</ThemeProvider>
+);
